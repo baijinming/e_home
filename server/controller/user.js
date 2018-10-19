0a1318384cf54ed3280248c41097c755671b14b0
baijinming <1225900529@qq.com>
2018-10-19 20:26:36 +0800

    e-home

--- server/controller/user.js
+++ server/controller/user.js
@@ -7,67 +7,81 @@ const tokenConfig = require('../config/tokenConfig');
 
 //添加用户接口
 router.post('/add', (req, res, next) => {
-    let {
-        avatar,
-        userName,
-        idCardNumber,
-        homeAddress,
-        workAddress,
-        nation,
-        weChat,
-        qqNumber,
-        sex,
-        education,
-        jobTitle,
-        emolument,
-        enterPartyTime,
-        paymentTime,
-        partyStatus,
-        pwd,
-        phone,
-        level
-    } = req.body;
-    user.findOne({idCardNumber}).then(data  => {
-        if(data === null) {
-            user.create({
-                avatar,
-                userName,
-                idCardNumber,
-                homeAddress,
-                workAddress,
-                nation,
-                weChat,
-                qqNumber,
-                sex,
-                education,
-                jobTitle,
-                emolument,
-                enterPartyTime,
-                paymentTime,
-                partyStatus,
-                pwd,
-                phone,
-                level
-            }).then(data => {
-                res.json({
-                    data: "success",
-                    code: 200,
-                    msg: '用户添加成功'
-                })
-            }).catch(err => {
-                let error = new Error(err);
-                next(error)
-            })
+    let token = req.headers.token || req.body.token;
+    jwt.verify(token, tokenConfig.secret, (err, decoded) => {
+        if(err) {
+            res.status(401).send("登录状态失效，请重新登录");
+            return
         }
-        else {
+       if(decoded.level.type == 0){
+           let {
+               avatar,
+               userName,
+               idCardNumber,
+               homeAddress,
+               workAddress,
+               nation,
+               weChat,
+               qqNumber,
+               sex,
+               education,
+               jobTitle,
+               emolument,
+               enterPartyTime,
+               paymentTime,
+               partyStatus,
+               pwd,
+               phone,
+               level
+           } = req.body;
+           user.findOne({idCardNumber}).then(data  => {
+               if(data === null) {
+                   user.create({
+                       avatar,
+                       userName,
+                       idCardNumber,
+                       homeAddress,
+                       workAddress,
+                       nation,
+                       weChat,
+                       qqNumber,
+                       sex,
+                       education,
+                       jobTitle,
+                       emolument,
+                       enterPartyTime,
+                       paymentTime,
+                       partyStatus,
+                       pwd,
+                       phone,
+                       level
+                   }).then(data => {
+                       res.json({
+                           data: "success",
+                           code: 200,
+                           msg: '用户添加成功'
+                       })
+                   }).catch(err => {
+                       let error = new Error(err);
+                       next(error)
+                   })
+               }
+               else {
+                   res.json({
+                       data: '用户名已存在',
+                       code: 400,
+                       msg: '用户名已存在'
+                   })
+               }
+           }).catch(error => {
+               next(new Error(error))
+           })
+       }else {
             res.json({
-                data: '用户名已存在',
                 code: 400,
-                msg: '用户名已存在'
+                msg: '你不是管理员，无法添加用户'
             })
-        }
-    }).catch(error => {
-        next(new Error(error))
+       }
     })
 })
 
@@ -106,12 +120,12 @@ router.post('/login', (req, res, next) => {
 //获取个人信息
 router.get('/userInfo', (req, res, next) => {
     let token = req.headers.token || req.body.token;
-    jwt.verify(token, tokenConfig.secret, (err, decode) =>{
+    jwt.verify(token, tokenConfig.secret, (err, decoded) =>{
         if(err) {
             res.status(401).send("登录状态失效，请重新登录");
             return
         }
-        user.findOne({_id: decode.id},{pwd: 0,level: 0,isCanLogin: 0}).then(data => {
+        user.findOne({_id: decoded.id},{pwd: 0,level: 0,isCanLogin: 0}).then(data => {
             res.json({
                 data,
                 code: 200,
